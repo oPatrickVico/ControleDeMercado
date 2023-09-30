4e945997570d59ea089ba7e4383818c4a415ab06
Alan Patrick <alan.patrick.fv@gmail.com>
2023-09-30 18:24:22 -0300

    test: Create test for newEntry

--- src/ListCreator/ListCreator.test.tsx
+++ src/ListCreator/ListCreator.test.tsx
@@ -1,14 +1,22 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
+
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
-describe('ListCreator', () => {
+beforeEach(() => {
   render(
     <MemoryRouter initialEntries={['/newList']}>
       <App />
     </MemoryRouter>
   );
+});
+
+describe('ListCreator', () => {
+  const user = userEvent.setup();
 
   it('loads', () => {
     expect(
@@ -18,5 +26,19 @@ describe('ListCreator', () => {
     expect(screen.getByLabelText(/Qtd:/i)).toBeDefined();
     expect(screen.getByLabelText(/Uni:/i)).toBeDefined();
     expect(screen.getByRole('button', { name: 'submit' })).toBeDefined();
+  });
+
+  it('adds new entry', async () => {
+    const newEntry = ['Banana', '3', 'unidades'];
+
+    user.type(screen.getByLabelText(/Nome/), newEntry[0]);
+    user.type(screen.getByLabelText(/Qtd/), newEntry[0]);
+    user.type(screen.getByLabelText(/Uni/), newEntry[0]);
+
+    await user.click(screen.getByRole('button', { name: /submit/ }));
+
+    expect(screen.getByText(/Banana/)).toBeVisible();
+    expect(screen.getByText(/3/)).toBeVisible();
+    expect(screen.getByText(/unidades/)).toBeVisible();
   });
 });
--- src/ListCreator/ListCreator.tsx
+++ src/ListCreator/ListCreator.tsx
@@ -3,18 +3,33 @@ export default function ListCreator() {
     <main>
       <h1>Criar uma nova lista:</h1>
       <form>
-        <label>
-          Nome:&nbsp;
-          <input type="text" name="name" />
-        </label>{' '}
-        <label>
-          Qtd:&nbsp;
-          <input type="text" name="amount" />
-        </label>{' '}
-        <label>
-          Uni:&nbsp;
-          <input type="text" name="unit" />
-        </label>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label>
+                  Nome:&nbsp;
+                  <input type="text" name="name" />
+                </label>
+              </td>
+              <td>
+                <label>
+                  Qtd:&nbsp;
+                  <input type="text" name="amount" />
+                </label>
+              </td>
+              <td>
+                <label>
+                  Uni:&nbsp;
+                  <input type="text" name="unit" />
+                </label>
+              </td>
+              <td>
+                <button aria-label="submit">+</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </form>
     </main>
   );
