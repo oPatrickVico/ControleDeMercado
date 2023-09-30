98caf5bba3752fbcbecdcbce72832a532d402849
Alan Patrick <alan.patrick.fv@gmail.com>
2023-09-30 02:04:46 -0300

    docs: Add explanation for <Routes> usage on <App />

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from './Dashboard';
 
 function App() {
   return (
+    // Usei Routes em vez dum router pois pretendo usar dois tipos: Um pra produção (BrowserRouter), outro pra testes (MemoryRouter). Usar Routes como root facilita os testes por deixar o componente portável (encaixa em qualquer lugar).
     <Routes>
       <Route path="/" element={<Dashboard />}>
         <Route path="test" element={<h1>Wazzuuuuup!</h1>} />
