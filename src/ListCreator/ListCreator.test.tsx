24aa0dbdba7cd8854b718081410e39e0a68c747e
Alan Patrick <alan.patrick.fv@gmail.com>
2023-09-30 19:20:02 -0300

    test: create labels object to facilitate swaping the text of the given elements

--- src/ListCreator/ListCreator.test.tsx
+++ src/ListCreator/ListCreator.test.tsx
@@ -15,6 +15,12 @@ beforeEach(() => {
   );
 });
 
+const labels = {
+  entryName: /Nome/i,
+  quantity: /Qtd/i,
+  units: /Uni/i,
+};
+
 describe('ListCreator', () => {
   const user = userEvent.setup();
 
@@ -22,18 +28,18 @@ describe('ListCreator', () => {
     expect(
       screen.getByRole('heading', { name: /Criar uma nova lista:/i })
     ).toBeDefined();
-    expect(screen.getByLabelText(/Nome:/i)).toBeDefined();
-    expect(screen.getByLabelText(/Qtd:/i)).toBeDefined();
-    expect(screen.getByLabelText(/Uni:/i)).toBeDefined();
+    expect(screen.getByLabelText(labels.entryName)).toBeDefined();
+    expect(screen.getByLabelText(labels.quantity)).toBeDefined();
+    expect(screen.getByLabelText(labels.units)).toBeDefined();
     expect(screen.getByRole('button', { name: 'submit' })).toBeDefined();
   });
 
   it('adds new entry', async () => {
     const newEntry = ['Banana', '3', 'unidades'];
 
-    await user.type(screen.getByLabelText(/Nome/), newEntry[0]);
-    await user.type(screen.getByLabelText(/Qtd/), newEntry[1]);
-    await user.type(screen.getByLabelText(/Uni/), newEntry[2]);
+    await user.type(screen.getByLabelText(labels.entryName), newEntry[0]);
+    await user.type(screen.getByLabelText(labels.quantity), newEntry[1]);
+    await user.type(screen.getByLabelText(labels.units), newEntry[2]);
 
     await user.click(screen.getByRole('button', { name: /submit/ }));
 
