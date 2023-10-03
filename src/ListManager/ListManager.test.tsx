efcf32e888061f727c784c5859520105ff722026
Alan Patrick <alan.patrick.fv@gmail.com>
2023-10-03 13:57:08 -0300

    test: New test for <ListManager>

---
+++ src/ListManager/ListManager.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+describe('ListManager', () => {
+  it('loads', () => {});
+});
