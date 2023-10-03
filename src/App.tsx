8b98db3705b9bb2105db33ce0fbafa4ee31f5710
Alan Patrick <alan.patrick.fv@gmail.com>
2023-10-03 13:54:14 -0300

    feat: Crete types for <ListManager>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { Dashboard } from './Dashboard';
 import ListEditor from './ListEditor/ListEditor';
+import React from 'react';
 
 function App() {
+  const [shoppingList, setShoppingLists] = React.useState<ShoppingList[]>();
+
+
   return (
     // Usei Routes em vez dum router pois pretendo usar dois tipos: Um pra produção (BrowserRouter), outro pra testes (MemoryRouter). Usar Routes como root facilita os testes por deixar o componente portável (encaixa em qualquer lugar).
     <Routes>
@@ -12,6 +16,19 @@ function App() {
     </Routes>
   );
 }
+
+enum ListStatus  {
+  pending,
+  done,
+  deleted
+}
+
+type ShoppingList = {
+  id: string;
+  createdAt: Date;
+  status: ListStatus;
+  list: ListEntry[];
+};
 
 export type ListEntry = {
   entryName: string;
